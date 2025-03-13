c9bb946fab264f38c114f79d6edd6374df48b53e
oscar martinez <rene2.martinez@outlook.com>
2025-03-13 14:15:20 -0700

    logs file fixed

--- src/infrastructure/datasources/file-system.datasource.ts
+++ src/infrastructure/datasources/file-system.datasource.ts
@@ -6,18 +6,18 @@ import { LogEntity, LogSeverityLevel } from "../../domain/entities/log.entity";
 
 export class FileSystemDatasource implements LogDatasource {
 
-    private readonly logPath = '/logs';
-    private readonly allLogsPath = '/logs/logs-low.log';
-    private readonly mediumLogsPath = '/logs/logs-medium.log';
-    private readonly highLogsPath = '/logs/logs-high.log';
+    private readonly logPath = 'logs/';
+    private readonly allLogsPath = 'logs/logs-low.log';
+    private readonly mediumLogsPath = 'logs/logs-medium.log';
+    private readonly highLogsPath = 'logs/logs-high.log';
 
     constructor(){
-        this.createLogsFiles;
+        this.createLogsFiles();
     }
 
     private createLogsFiles = () => {
         if ( !fs.existsSync( this.logPath ) ) {
-            fs.mkdirSync( this.logPath );
+            fs.mkdirSync( this.logPath, { recursive: true} );
         }
 
         [
